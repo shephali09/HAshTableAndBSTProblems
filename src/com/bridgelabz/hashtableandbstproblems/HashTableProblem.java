4b05fdb67e7af63ca24693e4370470d310a44b7e
shephali09 <hiremathshephali9@gmail.com>
2023-04-16 23:21:40 +0530

    Add: Ability to find frequency of words in a sentence

---
+++ src/com/bridgelabz/hashtableandbstproblems/HashTableProblem.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.hashtableandbstproblems;
+
+import java.util.*;
+
+public class HashTableProblem {
+
+	public static void main(String[] args) {
+
+		String sentence = "To be or not to be";
+
+		// split the sentemce into words
+		String[] words = sentence.split(" ");
+
+		Hashtable<String, Integer> table = new Hashtable<>();
+
+		// process each word in the sentence
+		for (String word : words) {
+
+			// check if word is already present in the hashtable
+			if (table.containsKey(word)) {
+
+				// if word already present then increase the count
+				int count = table.get(word);
+				table.put(word, count + 1);
+			} else {
+				// if word is not present in the hashtable, add it to hash table with count 1.
+				table.put(word, 1);
+			}
+		}
+		
+		//print the word and its count
+		Enumeration<String> keys = table.keys();
+		while(keys.hasMoreElements()){
+			String key = keys.nextElement();
+			int value = table.get(key);
+			System.out.println(key +" : " + value);
+			
+		}
+	}
+
+}
